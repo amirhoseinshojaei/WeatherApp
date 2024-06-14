7866f9446255d3fe0efa0b707e6df44caa2c86f9
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-06-14 20:52:25 +0330

    configure path url for index view

--- core/wethear/urls.py
+++ core/wethear/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
 ]
